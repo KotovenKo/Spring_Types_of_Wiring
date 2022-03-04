552d36e293fa129a3b5646d2da1eec5e78fa46d4
Vitaly <kotovenko.vitaly@gmail.com>
2022-03-04 14:30:48 +0300

    Added dependensies, tested.

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,12 @@
 package koschei.models;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Deth8 deth8;
 
     @Override
     public String toString() {
